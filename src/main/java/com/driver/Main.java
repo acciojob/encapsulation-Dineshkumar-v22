d6bea99c34ca8db4aaec72a704c3888c1a737457
Dinesh <dk159219@gmail.com>
2022-12-14 21:58:23 +0530

    q

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  RWOnly obj = new RWOnly();
+  //obj.m="Dineshkuma";
+  //'m' has private access in 'com.driver.RWOnly'
+obj.setName("Dinesh");
+System.out.println(obj.getName());
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String m= "Dineshkumar";
+    public String getName()
+    {
+        return m;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
 }
